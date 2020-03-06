2f0f624da2ccddc4179ccd759de3519eee744951
Alex Basystyi <alexbasisty@gmail.com>
2020-03-06 18:12:52 +0100

    inputs added, and rendering lists from json server/no add options

--- src/components/ToDoInputs.jsx
+++ src/components/ToDoInputs.jsx
@@ -1,27 +1,67 @@
 import React, { Component } from "react";
 
 class ToDoInputs extends Component {
+  state = {
+    title: '',
+    message: ''
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { title, message } = this.state;
+    const task = { ...this.state };
+
+    if(title.length > 0 && message.length > 0) {
+      fetch('http://localhost:3004/todo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+          .then(resp => resp.json())
+          .then(data => {
+            console.log('Success: ', data);
+          })
+          .catch(error => {
+            console.log('Error: ', error);
+          })
+    }
+  };
+
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
 
   render() {
+    const { title, message } = this.state;
+
     return (
         <div className="field">
           <form>
             <label className="label">New task title</label>
             <div className="control">
-              <input className="input" type="text" placeholder="task title" />
+              <input className="input" name="title" type="text" onChange={this.handleInput} placeholder="task title" value={title}/>
             </div>
             <div className="field">
               <label className="label">Message</label>
               <div className="control">
-                <textarea className="textarea" placeholder="Textarea" rows="10"/>
+                <textarea className="textarea" name="message" onChange={this.handleInput} placeholder="Textarea" value={message} rows="10"/>
               </div>
             </div>
             <div className="field is-grouped">
               <div className="control">
-                <button className="button is-link">Add</button>
+                <button className="button is-link" type="submit" onClick={this.handleSubmit}>Add</button>
               </div>
               <div className="control">
-                <button className="button is-link is-light">Cancel</button>
+                <button className="button is-link is-light" onClick={
+                  () => this.setState({
+                    title: '',
+                    message: ''
+                  })}>Cancel</button>
               </div>
             </div>
           </form>
